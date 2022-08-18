d6898a242fe0b65b823ed7d902b47f0d9785bdd0
AlexandrKryu <a.kryu84@gmail.com>
2022-08-19 00:25:52 +0300

    commit Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumNum(int num)
+{
+    int res = 0;
+    for (int i = 1; i < num; i++)
+    {
+        res = res + num % 10;
+        num = num / 10;
+    }
+    return res;
+}
+int result = SumNumNum(number);
+Console.WriteLine($"{number} -> {result}");
